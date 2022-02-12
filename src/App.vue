6b1ff8717789bd7fade3893b2fbbc0298ec50e22
zhu-hong <zhxhyy@qq.com>
2022-02-12 10:29:23 +0800

    init style

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,25 @@
 </script>
 
 <template>
+  <main class="w-full min-w-2xl min-h-screen"></main>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-thumb {
+  @apply bg-gray-500;
+  border-radius: 5px;
+
+  &:hover {
+    @apply bg-gray-600;
+  }
+}
+::-webkit-scrollbar-corner,
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     vue({
       reactivityTransform: true,
     }),
+    WindiCSS(),
   ],
 })
